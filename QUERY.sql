f82f1d375f558b8a8fc750fa9319b2e85f3239be
sanjidarimi <sanjidarimi312@gmail.com>
2026-07-05 23:08:42 +0600

    feat:using ilike and like for searching in user table

--- QUERY.sql
+++ QUERY.sql
@@ -2,3 +2,7 @@
 select match_id, fixture, base_ticket_price::integer from matches
 where tournament_category = 'Champions League'
 and match_status = 'Available';
+
+-- query 2
+
+select user_id,full_name, email from users where full_name like 'Tanvir%' or full_name ilike '%Haque%'
