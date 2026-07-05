5c288d2ecf9f530b6d5bc7dad4df0648b893e79f
sanjidarimi <sanjidarimi312@gmail.com>
2026-07-05 23:23:23 +0600

    feat:retrive booking table for change status

--- QUERY.sql
+++ QUERY.sql
@@ -6,3 +6,9 @@ and match_status = 'Available';
 -- query 2
 
 select user_id,full_name, email from users where full_name like 'Tanvir%' or full_name ilike '%Haque%'
+
+-- query 3
+select booking_id ,user_id,match_id,
+  coalesce (payment_status,'Action Required') 
+  as systematic_status from bookings
+  where payment_status is null 
