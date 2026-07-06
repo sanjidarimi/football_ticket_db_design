d77de83dac7807c64d1e6cca4177bd592ad0d646
sanjidarimi <sanjidarimi312@gmail.com>
2026-07-06 22:41:16 +0600

    feat:implement inner join query

--- QUERY.sql
+++ QUERY.sql
@@ -12,3 +12,8 @@ select booking_id ,user_id,match_id,
   coalesce (payment_status,'Action Required') 
   as systematic_status from bookings
   where payment_status is null 
+
+-- query 4
+select booking_id,full_name,fixture,total_cost::integer from bookings 
+inner join users on bookings.user_id = users.user_id
+inner join matches on bookings.match_id = matches.match_id
