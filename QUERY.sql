49e866371bc89000499fdbcdfb596f4c585b1025
sanjidarimi <sanjidarimi312@gmail.com>
2026-07-06 23:16:00 +0600

    feat:added query 5.6.7

--- QUERY.sql
+++ QUERY.sql
@@ -17,3 +17,15 @@ select booking_id ,user_id,match_id,
 select booking_id,full_name,fixture,total_cost::integer from bookings 
 inner join users on bookings.user_id = users.user_id
 inner join matches on bookings.match_id = matches.match_id
+
+-- query 5 
+select users.user_id, full_name, booking_id from users
+left join bookings on users.user_id = bookings.user_id
+  
+--query 6
+select booking_id, match_id, total_cost::integer from bookings 
+where total_cost > (select avg(total_cost) from bookings)
+
+-- query 7 
+select match_id, fixture, base_ticket_price::integer from matches
+order by base_ticket_price desc limit 2 offset 1
